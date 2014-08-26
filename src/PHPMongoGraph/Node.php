2712e33bb43403d2d5c2d0c1285cdc4b7383ede6
anu <anuradha.j@sorewarding.com>
2014-08-26 11:52:33 +0530

    save ts

--- src/PHPMongoGraph/Node.php
+++ src/PHPMongoGraph/Node.php
@@ -5,12 +5,28 @@ Class Node{
 	private $_data;
 	private $_mongoId;
 	
-	public function getConnections($types, $direction){
+	public function getConnections($types, $direction, $after = null, $limit = 30){
 		$d = ($direction == Connection::DIRECTION_IN) ? 'end' : 'start';
 		$q = array($d => $this->_mongoId, 'type' => array('$in' => $types));
-		$cur = MClient::getDb()->find(Graph::$_relscoll, $q, array('hash' => false));
 		
-		return new Relationships($this->getId(), $cur, $direction, $types);
+		if(!is_null($after)){
+			$q['uts'] = array('$lt' => new \MongoDate($after));
+		}
+		
+		$cur = MClient::getDb()->find(Graph::$_relscoll, $q, array('hash' => false))->sort(array('uts' => -1))->limit($limit);
+		
+		return $cur;
+		
+		/*$result = MClient::getDb()->aggregate(Graph::$_relscoll, array(
+				array('$match' => array($d => $this->_mongoId, 'type' => array('$in' => $types))),
+				array('$group' => array('_id' => '$end', 'doc' => array('$addToSet' => '$_id')))
+		));
+		
+		print_r($result['result']);
+		exit();
+		
+		return $result['result'];*/
+		//return new Relationships($this->getId(), $cur, $direction, $types);
 	}
 	
 	public function getId(){
@@ -34,6 +50,7 @@ Class Node{
 	}
 	
 	public function save(){
+		$this->_data['uts'] = new \MongoDate(time());
 		MClient::getDb()->update(
 				Graph::$_nodescoll,
 				array('_id' => $this->_mongoId),
@@ -52,7 +69,7 @@ Class Node{
 		}
 		else{
 			$this->_mongoId = new \MongoId();
-			$this->_data = array();
+			$this->_data = array('cts' => new \MongoDate(time()));
 			$this->_data['props'] = array();
 		}		
 	}
